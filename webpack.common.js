a487ba04ff56bc474e18faeea1206c6e6cf2534f
korot0 <108902180+korot0@users.noreply.github.com>
2025-03-07 22:47:11 -0600

    Delete webpack.config.js and merge common dev and prod configurations

--- webpack.common.js
+++ webpack.common.js
@@ -3,19 +3,15 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
-  devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["./src/index.html"],
-  },
   plugins: [
     new HtmlWebpackPlugin({
+      title: "Production",
       template: "./src/index.html",
     }),
   ],
